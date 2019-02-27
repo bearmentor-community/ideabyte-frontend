0bd8f044487cdd4c01cd385a996e9d62b88d0f7d
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-28 05:51:23 +0700

    Improve reducers documentation

--- src/redux/reducers/initial.js
+++ src/redux/reducers/initial.js
@@ -6,22 +6,35 @@ const initialState = {
   latestError: null,
 
   //////////////////////////////////////////////////////////////////////////////
-  // will be used if still loading
+  // will be used if still loading somewhere
   isLoading: false,
   // will be used if data fetching to API is still ongoing
   isFetching: false,
 
+  //////////////////////////////////////////////////////////////////////////////
   // before we're logged in, the auth/admin conditions are false
   isAuthenticated: false, // will be true after we're logged in
-  isAdmin: false, // will be true after we're logged in as admin
-
-  //////////////////////////////////////////////////////////////////////////////
-  // after we're logged in, we have a JSON Web Token (JWT)
-  token: null // a string of JWT
+  isAdmin: false // will be true after we're logged in as admin
 }
 
+// Reducers are pure functions
+// They should not have any side effects nor mutate the state
+// They have to return a copy
 const initialReducer = (state = initialState, action) => {
   switch (action.type) {
+    ////////////////////////////////////////////////////////////////////////////
+    // SAMPLE ACTION TYPE
+    case 'GET_STATE': {
+      // remember, a reducer would not modify the data directly
+      // instead, we return a copy
+      return {
+        // simpler to retrieve all other keys in state with spread operator
+        // so we don't have to specify each keys on by one
+        ...state,
+        state: state
+      }
+    }
+
     default: {
       // ALWAYS have a default case in a reducer
       return state
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,6 +3,12 @@ import ObjectID from 'bson-objectid'
 //////////////////////////////////////////////////////////////////////////////
 // this specific user will be retrieved from the backend /users/:id
 const initialState = {
+  //////////////////////////////////////////////////////////////////////////////
+  // after we're logged in, we have a JSON Web Token (JWT)
+  token: null, // a string of JWT
+
+  //////////////////////////////////////////////////////////////////////////////
+  // we can also interpolate the decoded token to plain object
   _id: ObjectID(),
   id: 1,
   avatar: '/assets/images/avatar.jpg',
@@ -10,23 +16,8 @@ const initialState = {
   email: 'joendoe@example.com'
 }
 
-// Reducers are pure functions
-// They should not have any side effects nor mutate the state
-// They have to return a copy
-export default function otherReducer(state = initialState, action) {
+export default function userReducer(state = initialState, action) {
   switch (action.type) {
-    ////////////////////////////////////////////////////////////////////////////
-    case 'GET_USER': {
-      // remember, a reducer would not modify the data directly
-      // instead, we return a copy
-      return {
-        // simpler to retrieve all other keys in state with spread operator
-        // so we don't have to specify each keys on by one
-        ...state,
-        user: state
-      }
-    }
-
     ////////////////////////////////////////////////////////////////////////////
     default: {
       return state
