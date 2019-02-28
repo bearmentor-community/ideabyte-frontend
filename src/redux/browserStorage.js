808ce2a292ec62cfc138bb07795ef90354860cbc
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-28 07:06:57 +0700

    Better storage handling with JSON.parse/stringify

--- src/redux/browserStorage.js
+++ src/redux/browserStorage.js
@@ -4,13 +4,13 @@ const browserStorage = {
   // Set key's value into storage
   // doesn't need to return
   setKey: (keyName, value) => {
-    localStorage.setItem(keyName, value)
+    localStorage.setItem(keyName, JSON.stringify(value))
   },
 
   // Get key's value from storage
   // return the value
   getKey: keyName => {
-    return localStorage.getItem(keyName)
+    return JSON.parse(localStorage.getItem(keyName))
   },
 
   // Remove key from the storage
