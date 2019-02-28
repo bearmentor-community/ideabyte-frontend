597240bf5a74f88916a8a5c6f4f447e83c1e1c33
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-28 08:09:57 +0700

    Prepare for idea & post actions

---
+++ src/redux/actions/idea.js
@@ -0,0 +1,55 @@
+import { push } from 'connected-react-router'
+
+import request from '../request'
+import browserStorage from '../browserStorage'
+
+export const getIdeasBegin = () => ({
+  type: 'GET_IDEAS_BEGIN'
+})
+
+export const getIdeasSuccess = response => {
+  return {
+    type: 'GET_IDEAS_SUCCESS',
+    payload: {
+      response
+    }
+  }
+}
+
+export const getIdeasError = error => ({
+  type: 'GET_IDEAS_ERROR',
+  payload: {
+    error
+  }
+})
+
+export const removeUserState = () => ({
+  type: 'REMOVE_USER_STATE'
+})
+
+// getIdeas is a thunk
+export const getIdeas = payload => {
+  return dispatch => {
+    dispatch(getIdeasBegin())
+
+    // Get the response after requesting to backend API
+    // Use Promise instead of async/await because it's tricky in thunk
+    request({
+      method: 'get',
+      url: '/ideas'
+    })
+      .then(response => {
+        dispatch(getIdeasSuccess(response))
+      })
+      .catch(error => {
+        dispatch(getIdeasError(error))
+        toast.error(`Sorry ${payload.name}, there's something wrong`, {
+          position: 'top-center',
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: false
+        })
+      })
+  }
+}
---
+++ src/redux/actions/post.js
@@ -0,0 +1,49 @@
+import { push } from 'connected-react-router'
+
+import request from '../request'
+import browserStorage from '../browserStorage'
+
+export const postNewIdeaBegin = () => ({
+  type: 'POST_NEW_IDEA_BEGIN'
+})
+
+export const postNewIdeaSuccess = response => {
+  return {
+    type: 'POST_NEW_IDEA_SUCCESS',
+    payload: {
+      response
+    }
+  }
+}
+
+export const postNewIdeaError = error => ({
+  type: 'POST_NEW_IDEA_ERROR',
+  payload: {
+    error
+  }
+})
+
+// postNewIdea is a thunk
+export const postNewIdea = payload => {
+  return dispatch => {
+    dispatch(postNewIdeaBegin())
+
+    request({
+      method: 'post',
+      url: '/ideas'
+    })
+      .then(response => {
+        dispatch(postNewIdeaSuccess(response))
+      })
+      .catch(error => {
+        dispatch(postNewIdeaError(error))
+        toast.error(`Sorry ${payload.name}, there's something wrong`, {
+          position: 'top-center',
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: false
+        })
+      })
+  }
+}
