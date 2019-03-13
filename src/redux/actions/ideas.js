4414f2bb58e4c0c28dee781696d20e252022331a
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-13 16:11:02 +0700

    Get actual ideas data from the backend

---
+++ src/redux/actions/ideas.js
@@ -0,0 +1,54 @@
+import { toast } from 'react-toastify'
+
+import request from '../request'
+
+export const getIdeasBegin = () => ({
+  type: 'GET_IDEAS_BEGIN'
+})
+
+export const getIdeasSuccess = response => {
+  return {
+    type: 'GET_IDEAS_SUCCESS',
+    payload: {
+      response
+    }
+  }
+}
+
+export const getIdeasError = error => ({
+  type: 'GET_IDEAS_ERROR',
+  payload: {
+    error
+  }
+})
+
+export const removeUserState = () => ({
+  type: 'REMOVE_USER_STATE'
+})
+
+// getIdeas is a thunk
+export const getIdeas = payload => {
+  return dispatch => {
+    dispatch(getIdeasBegin())
+
+    // Get the response after requesting to backend API
+    // Use Promise instead of async/await because it's tricky in thunk
+    request({
+      method: 'get',
+      url: '/ideas'
+    })
+      .then(response => {
+        dispatch(getIdeasSuccess(response))
+      })
+      .catch(error => {
+        dispatch(getIdeasError(error))
+        toast.error(`Sorry ${payload.name}, there's something wrong`, {
+          position: 'top-left',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: false
+        })
+      })
+  }
+}
--- src/redux/reducers/ideas.js
+++ src/redux/reducers/ideas.js
@@ -1,67 +1,39 @@
-import ObjectID from 'bson-objectid'
+const initialState = {
+  latestResponse: null,
+  latestError: null,
+  isLoading: false,
+  data: []
+  // the ideas will be retrieved from the backend API /ideas
+}
 
-//////////////////////////////////////////////////////////////////////////////
-// these ideas will be retrieved from the backend /ideas
-const initialState = [
-  {
-    _id: ObjectID(),
-    id: 1,
-    title: 'Tripvesto Trip Planner 1',
-    description:
-      "App to plan and gather your friends to travel. Let's join us!",
-    author: 'Joen Doe',
-    date: '2019/02/25',
-    location: 'Jakarta, Indonesia',
-    slug: 'tripvesto-trip-planner',
-    images: [
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg'
-    ],
-    details: ''
-  },
-  {
-    _id: ObjectID(),
-    id: 2,
-    title: 'Tripvesto Trip Planner 2',
-    description:
-      "App to plan and gather your friends to travel. Let's join us!",
-    author: 'Joen Doe',
-    date: '2019/02/25',
-    location: 'Jakarta, Indonesia',
-    slug: 'tripvesto-trip-planner',
-    images: [
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg'
-    ],
-    details: ''
-  },
-  {
-    _id: ObjectID(),
-    id: 3,
-    title: 'Tripvesto Trip Planner 3',
-    description:
-      "App to plan and gather your friends to travel. Let's join us!",
-    author: 'Joen Doe',
-    date: '2019/02/25',
-    location: 'Jakarta, Indonesia',
-    slug: 'tripvesto-trip-planner',
-    images: [
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg',
-      '/assets/images/traveling.jpg'
-    ],
-    details: ''
-  }
-]
-
-function ideasReducer(state = initialState, action) {
+const ideasReducer = (state = initialState, action) => {
   switch (action.type) {
     ////////////////////////////////////////////////////////////////////////////
-    case 'GET_IDEAS': {
+    case 'GET_IDEAS_BEGIN': {
       return {
-        ideas: state.ideas
+        ideas: state.ideas,
+        isLoading: true,
+        latestError: null
+      }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    case 'GET_IDEAS_SUCCESS': {
+      return {
+        ...state,
+        isLoading: false,
+        latestResponse: action.payload.response,
+        data: action.payload.response.data.ideas
+      }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    case 'GET_IDEAS_ERROR': {
+      return {
+        ...state,
+        isLoading: false,
+        latestError: action.payload.error,
+        latestResponse: null
       }
     }
 
